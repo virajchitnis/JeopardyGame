75189be97fabcd1fe3a52337df857035204acee5
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-23 15:35:39 -0400

    Added basic design layout to index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <html>
 	<head>
-		<title>Welcome to Jeopardy Game!</title>
+		<link rel="stylesheet" href="css/design.css">
+		<title>Welcome to Jeopardy!</title>
 	</head>
+	<body>
+		<div class="wrapper">
+			<h2>Jeopardy!</h2>
+			<div class="push"></div>
+		</div>
+		<div class="footer">
+			<p>&copy; 2014 Viraj Chitnis</p>
+		</div>
+	</body>
 </html>
