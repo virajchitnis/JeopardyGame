8c4a40855a37306b535ce8d4dab17d93dc26cb84
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-24 19:53:42 -0400

    Created setup div
    Created the setup div in game.php. This is the initial div that shows up when the game form is loaded with the appropriate data. In this div, the user can set the amount of time given to each team/player and the names for as many teams as are playing.

--- game.php
+++ game.php
@@ -12,9 +12,30 @@
 			exec("rm ".$location.$deflated_file);
 		?>
 		<link rel="stylesheet" href="css/design.css">
+		<script src="js/game.js"></script>
 		<title><?php echo $open_file; ?> - Play Jeopardy!</title>
 	</head>
-	<body>
+	<body id="body_background">
+		<div id="setup_div">
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<p>&nbsp;</p>
+			<h3>Time per team per question:</h3>
+			<form id="timer_form">
+				<input type="text" name="timer" placeholder="Time" size="4">
+			</form>
+			<p>&nbsp;</p>
+			<h3>Teams:</h3>
+			<form id="team_form">
+				<input type="text" name="1" placeholder="Team 1" size="25" maxlength="25">
+				<input type="text" name="2" placeholder="Team 2" size="25" maxlength="25">
+			</form>
+			<p>&nbsp;</p>
+			<button onclick="addTeam()">Add more</button>
+			<button>Start game</button>
+		</div>
 		<p id="hidden_json"><?php echo $json; ?></p>
 	</body>
 </html>
---
+++ js/game.js
@@ -0,0 +1,26 @@
+function addTeam () {
+	var parent = document.getElementById('team_form');
+	var previous = parseInt(parent.elements[parent.length - 1].name);
+	var current = previous + 1;
+	
+	var breakLine = document.createElement('br');
+	var breakLine1 = document.createElement('br');
+	var space = document.createElement('span');
+	space.innerHTML = " ";
+	
+	var teamText = document.createElement('input');
+	teamText.setAttribute('type', 'text');
+	teamText.setAttribute('name', current);
+	teamText.setAttribute('placeholder', 'Team ' + current);
+	teamText.setAttribute('size', '25');
+	teamText.setAttribute('maxlength', '25');
+	
+	if ((previous % 2) == 0) {
+		parent.appendChild(breakLine);
+		parent.appendChild(breakLine1);
+	}
+	else {
+		parent.appendChild(space);
+	}
+	parent.appendChild(teamText);
+}
