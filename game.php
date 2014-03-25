81deb114c1a5368a56eae1caf4293a98bf5774a6
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-24 20:12:19 -0400

    Remove file extension from file name on the fly
    Game.php can remove the file extension from the file name on the fly. This is necessary because the file extension gets removed by gunzip when it decompresses the file.

--- game.php
+++ game.php
@@ -3,13 +3,12 @@
 		<?php
 			$file = "test.qbk";
 			$open_file = "test_open.qbk";
-			$deflated_file = "test_open";
 			$location = "/tmp/";
 			
 			exec("cp ".$location.$file." ".$location.$open_file);
 			exec("gunzip -S .qbk ".$location.$open_file);
-			$json = file_get_contents($location.$deflated_file);
-			exec("rm ".$location.$deflated_file);
+			$json = file_get_contents($location.basename($open_file, ".qbk"));
+			exec("rm ".$location.basename($open_file, ".qbk"));
 		?>
 		<link rel="stylesheet" href="css/design.css">
 		<script src="js/game.js"></script>
