37a6609f0ec4dca25619ccb8ec06064480bf6ff5
Viraj Chitnis <chitnisviraj@gmail.com>
2014-03-31 16:11:45 -0400

    Version info in footer
    The footer now displays version info.

--- common/footer.php
+++ common/footer.php
@@ -1,3 +1,4 @@
 <div class="footer">
-	<p class="footer_text">&copy; 2014 Viraj Chitnis</p>
+	<p class="footer_text"><?php echo exec("git describe"); ?></p>
+	<p class="footer_text">Viraj Chitnis</p>
 </div>
