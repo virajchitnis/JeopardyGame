cae5dea657c6f6bcb1cc376b86e0a4c01ab30427
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-02 19:32:15 -0400

    Question bank creator checks point validity
    The question bank creator checks if the entered point values are valid and in the proper order.

--- js/index.js
+++ js/index.js
@@ -58,6 +58,32 @@ function createJSON () {
 	var dataForm = document.getElementById('question_form');
 	var questions = new Array();
 	
+	if (!((parseInt(dataForm.elements[3].value) < parseInt(dataForm.elements[6].value)) && (parseInt(dataForm.elements[6].value) < parseInt(dataForm.elements[9].value)) && (parseInt(dataForm.elements[9].value) < parseInt(dataForm.elements[12].value)) && (parseInt(dataForm.elements[12].value) < parseInt(dataForm.elements[15].value)))) {
+		alert("Points in each category have to be in ascending order.");
+		return false;
+	}
+	
+	if (!((dataForm.elements[3].value == dataForm.elements[19].value) && (dataForm.elements[19].value == dataForm.elements[35].value) && (dataForm.elements[35].value == dataForm.elements[51].value) && (dataForm.elements[51].value == dataForm.elements[67].value) && (dataForm.elements[67].value == dataForm.elements[83].value))) {
+		alert("Each category has to have the same point values and they have to be in the same order.");
+		return false;
+	}
+	if (!((dataForm.elements[6].value == dataForm.elements[22].value) && (dataForm.elements[22].value == dataForm.elements[38].value) && (dataForm.elements[38].value == dataForm.elements[54].value) && (dataForm.elements[54].value == dataForm.elements[70].value) && (dataForm.elements[70].value == dataForm.elements[86].value))) {
+		alert("Each category has to have the same point values and they have to be in the same order.");
+		return false;
+	}
+	if (!((dataForm.elements[9].value == dataForm.elements[25].value) && (dataForm.elements[25].value == dataForm.elements[41].value) && (dataForm.elements[41].value == dataForm.elements[57].value) && (dataForm.elements[57].value == dataForm.elements[73].value) && (dataForm.elements[73].value == dataForm.elements[89].value))) {
+		alert("Each category has to have the same point values and they have to be in the same order.");
+		return false;
+	}
+	if (!((dataForm.elements[12].value == dataForm.elements[28].value) && (dataForm.elements[28].value == dataForm.elements[44].value) && (dataForm.elements[44].value == dataForm.elements[60].value) && (dataForm.elements[60].value == dataForm.elements[76].value) && (dataForm.elements[76].value == dataForm.elements[92].value))) {
+		alert("Each category has to have the same point values and they have to be in the same order.");
+		return false;
+	}
+	if (!((dataForm.elements[15].value == dataForm.elements[31].value) && (dataForm.elements[31].value == dataForm.elements[47].value) && (dataForm.elements[47].value == dataForm.elements[63].value) && (dataForm.elements[63].value == dataForm.elements[79].value) && (dataForm.elements[79].value == dataForm.elements[95].value))) {
+		alert("Each category has to have the same point values and they have to be in the same order.");
+		return false;
+	}
+	
 	var currCategory = "";
 	var i = 0;
 	while (i < dataForm.length) {
