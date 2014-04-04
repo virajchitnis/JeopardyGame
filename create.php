2b64d57f4ea4995fdb1e4ab1c5933cc236224022
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-04 19:16:33 -0400

    Moved creator to separate page and redesigned
    The question bank creator has been moved to a separate page and it has been redesigned.

---
+++ create.php
@@ -0,0 +1,205 @@
+<html>
+	<head>
+		<link rel="stylesheet" href="css/design.css">
+		<script src="js/index.js"></script>
+		<script src="js/types.js"></script>
+		<script src="js/openlink.js"></script>
+		<title>Create a question bank - Jeopardy!</title>
+	</head>
+	<body class="body">
+		<div class="wrapper">
+			<?php include('common/header.php'); ?>
+			<div id="creategame_body">
+				<h3>Create a question bank:</h3>
+				<p>&nbsp;</p>
+				<form id="name_form">
+					<input type="text" name="name" placeholder="Question bank name" size="25" maxlength="25">
+					<input type="text" name="author" placeholder="Author name" size="25" maxlength="25">
+				</form>
+				<p>&nbsp;</p>
+				<form id="question_form">
+					<input type="text" name="category" placeholder="Category">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<br>
+					<input type="text" name="category" placeholder="Category">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<br>
+					<input type="text" name="category" placeholder="Category">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<br>
+					<input type="text" name="category" placeholder="Category">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<br>
+					<input type="text" name="category" placeholder="Category">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<br>
+					<input type="text" name="category" placeholder="Category">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<p class="question_form_spaces">&nbsp;</p>
+					<input type="text" name="question" placeholder="Question" size="50">
+					<input type="text" name="answer" placeholder="Answer" size="25">
+					<input type="text" name="weight" placeholder="Weight" size="6">
+					<br>
+					<br>
+				</form>
+				<div>
+					<form class="create_game_buttons" id="done_button" method="post" action="scripts/storejson.php" onsubmit=" return createJSON()">
+						<input type="hidden" name="json" value="">
+						<input type="hidden" name="name" value="">
+						<input type="hidden" name="author" value="">
+						<input type="submit" value="Done">
+					</form>
+					<button class="create_game_buttons" onclick="openLink('./')" id="back_button">Go back</button>
+				</div>
+			</div>
+			<p>&nbsp;</p>
+			<div class="push"></div>
+		</div>
+		<?php include('common/footer.php'); ?>
+	</body>
+</html>
--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-function showCreatePage () {
-	document.getElementById('welcome_body').style.display = 'none';
-	document.getElementById('creategame_body').style.display = 'block';
-}
-
-function showWelcomePage () {
-	document.getElementById('creategame_body').style.display = 'none';
-	document.getElementById('welcome_body').style.display = 'block';
-}
-
 function addQuestion () {
 	var parent = document.getElementById('question_form');
 	
