0b1f5ac4cceaf1ece79ca9bde66fd4e93286442f
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-27 18:22:10 -0400

    Removed version info from footer
    The distributed version of this application will not be a git repository, so retrieving the version via git will not work.

--- src/common/footer.php
+++ src/common/footer.php
@@ -1,13 +1,5 @@
 <div class="footer">
 	<p class="footer_text">
-		Jeopardy!
-		<span class="footer_text">
-			<?php
-			if (!file_exists('deployed')) {
-				echo exec("git describe");
-			}
-			?>
-		</span>
-		by <a id="footer_link" href="http://www.virajchitnis.com">Viraj Chitnis</a>
+		Jeopardy! by <a id="footer_link" href="http://www.virajchitnis.com">Viraj Chitnis</a>
 	</p>
 </div>
