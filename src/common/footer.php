1affe8d28ec54e238266e0b14c64377c13ce9e13
Viraj Chitnis <chitnisviraj@gmail.com>
2014-04-28 01:29:25 -0400

    Removed version info from footers
    Removed version info from footers and forced vagrant destroy.

--- src/common/footer.php
+++ src/common/footer.php
@@ -1,5 +1,3 @@
 <div class="footer">
-	<p class="footer_text">
-		Jeopardy! <?php echo get_file_contents("version") ?> by <a id="footer_link" href="http://www.virajchitnis.com">Viraj Chitnis</a>
-	</p>
+	<p class="footer_text">Jeopardy! by <a id="footer_link" href="http://www.virajchitnis.com">Viraj Chitnis</a></p>
 </div>
