026d1cb06f7d53406a01c7545d9901448ef756e0
Viraj Chitnis <chitnisviraj@gmail.com>
2014-05-02 03:22:31 -0400

    Added license and GitHub info to the footer
    The footer of every page now has info and links to the license and the footer

--- src/common/footer.php
+++ src/common/footer.php
@@ -1,3 +1,4 @@
 <div class="footer">
 	<p class="footer_text">Jeopardy! by <a id="footer_link" href="http://www.virajchitnis.com">Viraj Chitnis</a></p>
+	<p class="footer_text">This project is <a href="https://github.com/virajchitnis/JeopardyGame">developed on GitHub</a> and is available under the <a href="https://github.com/virajchitnis/JeopardyGame/blob/master/LICENSE.md">MIT License</a></p>
 </div>
